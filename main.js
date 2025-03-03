f4e00bc95d954685d4bb6474e716e85371652da5
aprxiaoyang <aprxiaoyang@163.com>
2025-03-03 12:27:33 +0800

    2025年3月3日12:26:51 --- 添加uview组件库

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+
+// 引入全局uView
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,15 @@
 <template>
-	<view class="flex flex-col space-y-6 mx-auto items-center">
+	<view class="flex flex-col space-y-6 mx-auto items-center mt-7">
 		<view>
 			<image class="w-20 h-20" src="/static/logo.png"></image>
 		</view>
 
-		<view class="">
-			<text class="bg-red-300 text-white">{{ title }}</text>
+		<view class="title">
+			<text class="text-white text-3xl">{{ title }}</text>
+		</view>
+		
+		<view class="notice-bar">
+			<u-notice-bar text="一曲肝肠断,天涯何处觅知音."></u-notice-bar>
 		</view>
 	</view>
 </template>
@@ -26,4 +30,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.title {
+	background-color: orange;
+}
+.notice-bar {
+	background-color: red;
+}
+</style>
